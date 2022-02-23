3727ed263914c2565fec647181b3d43bbd6559b3
Cameron Fraser <cameronsfraser@gmail.com>
2022-02-23 19:51:09 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -9,32 +9,24 @@ get "/" do
     erb(:index)
 end
 
-get "/finstagram_posts/:id" do
-    @finstagram_post = FinstagramPost.find(params[:id])
-    erb(:"finstagram_posts/show")
-end
-
-get "/finstagram_posts/new" do
-    @finstagram_post = FinstagramPost.new
-    erb(:"finstagram_posts/new")
-end
-
-post "/finstagram_posts" do
-    photo_url = params[:photo_url]
-
-    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
-
-    if @finstagram_post.save
-        redirect(to("/"))
-    else
-        erb(:"finstagram_posts/new")
-    end
-end
-
-
 get "/signup" do
     @user = User.new
     erb(:signup)
+end
+
+post "/signup" do
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+
+    if @user.save
+        redirect to("/login")
+    else
+        erb(:signup)
+    end
 end
 
 get "/login" do
@@ -61,17 +53,56 @@ get "/logout" do
     redirect to("/")
 end
 
-post "/signup" do
-    email = params[:email]
-    avatar_url = params[:avatar_url]
-    username = params[:username]
-    password = params[:password]
-
-    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
-
-    if @user.save
+before "/finstagram_posts/new" do
+    if !current_user
         redirect to("/login")
-    else
-        erb(:signup)
     end
+end
+
+get "/finstagram_posts/new" do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+post "/finstagram_posts" do
+    photo_url = params[:photo_url]
+
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+    if @finstagram_post.save
+        redirect(to("/"))
+    else
+        erb(:"finstagram_posts/new")
+    end
+end
+
+get "/finstagram_posts/:id" do
+    @finstagram_post = FinstagramPost.find(params[:id])
+    erb(:"finstagram_posts/show")
+end
+
+post "/comments" do
+    text = params[:text]
+    finstagram_post_id = params[:finstagram_post_id]
+
+    comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+    comment.save
+
+    redirect(back)
+end
+
+post "/likes" do
+    finstagram_post_id = params[:finstagram_post_id]
+    
+    like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id})
+    like.save
+
+    redirect(back)
+end
+
+delete "/likes/:id" do
+    like = Like.find(params[:id])
+    like.destroy
+    redirect(back)
 end
